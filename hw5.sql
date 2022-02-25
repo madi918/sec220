c4a8e73ff1d4f090fc575f8269447709b1473558
madi918 <98625452+madi918@users.noreply.github.com>
2022-02-25 13:26:10 -0500

    hw5

---
+++ hw5.sql
@@ -0,0 +1,38 @@
+create table customers
+(
+    id        int auto_increment
+        primary key,
+    firstname varchar(20) null,
+    lastname  varchar(20) null,
+    company   varchar(20) null,
+    constraint id
+        unique (id)
+);
+
+create table customers_order_association
+(
+    id           int auto_increment
+        primary key,
+    customers_id int not null,
+    orders_id    int not null,
+    constraint id
+        unique (id)
+);
+
+create table orders
+(
+    id         int auto_increment
+        primary key,
+    product    varchar(20) null,
+    cost       int         null,
+    customerId int         null,
+    constraint id
+        unique (id),
+    constraint orders_ibfk_1
+        foreign key (customerId) references customers (id)
+);
+
+create index customerId
+    on orders (customerId);
+
+
